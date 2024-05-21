4b544f818310568c3bfd4e2ef398565eff35ee00
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-05-21 09:30:33 +0300

    first commit. Security controller

---
+++ SocialNetwork/Controllers/SecurityController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Controllers
+{
+    public class SecurityController1 : Controller
+    {
+        public ActionResult Logon()
+        {
+            return View();
+        }
+    }
+}
